41cf8a53912c8738fa72e5092edb1a873c904c31
Max Boyko <mboyko@cs.toronto.edu>
2026-08-11 12:07:16 -0400

    * Change DDAH reminder email text to more detailed version * Fix Email DDAH Reminders action name text * Allow confirmation window for reminders on 1 selection

--- backend/app/mailers/position_mailer.rb
+++ backend/app/mailers/position_mailer.rb
@@ -3,21 +3,21 @@
 class PositionMailer < ActionMailer::Base
     require 'html_to_plain_text'
 
-    def email_ddah_reminder(position)
-        generate_vars(position)
+    def email_ddah_reminder(position, instructor = nil)
+        generate_vars(position, instructor)
 
         unless Rails.application.config.enable_emailing
-            logger.warn "ENABLE_EMAILING is not true; skipping email to \"#{@email}\""
+            logger.warn "ENABLE_EMAILING is not true; skipping email to \"#{@recipient_email}\""
             return
         end
 
         debug_message =
-            "Emailing DDAH reminder for #{@position_code} to #{@instructor_emails}"
+            "Emailing DDAH reminder for #{@position_code} to #{@recipient_email}"
         logger.warn debug_message
 
         begin
             mail(
-                to: @instructor_emails,
+                to: @recipient_email,
                 from: @ta_coordinator_email,
                 subject:
                     "DDAH Reminder for #{@position_code}"
@@ -40,20 +40,32 @@ class PositionMailer < ActionMailer::Base
         template.render(@subs.stringify_keys)
     end
 
-    def generate_vars(position)
+    def generate_vars(position, instructor = nil)
         @position = position
         @position_code = position.position_code
         @session = position.session
+        @faculty_names =
+            position.instructors.map do |position_instructor|
+                "#{position_instructor.first_name} #{position_instructor.last_name}".strip
+            end
         @instructor_emails = position.instructors.map(&:email)
+        @faculty_name =
+            if instructor.nil?
+                @faculty_names.to_sentence
+            else
+                "#{instructor.first_name} #{instructor.last_name}".strip
+            end
+        @recipient_email = instructor.nil? ? @instructor_emails : instructor.email
         @ta_coordinator_email = Rails.application.config.ta_coordinator_email
 
+        # Use the /hash proxy route so frontend hash routing survives authentication redirects.
         @url =
-            "#{Rails.application.config.base_url}/" \
-                "?role=%22instructor%22&activeSession=#{@session.id}" \
-                "#/positions/#{@position.id}/ddahs"
+            "#{Rails.application.config.base_url}/?role=%22instructor%22&activeSession=#{@session.id}" \
+                "/hash/positions/#{@position.id}/ddahs"
 
         @subs = {
             position: @position,
+            faculty_name: @faculty_name,
             instructor_emails: @instructor_emails,
             position_code: @position_code,
             session_name: @session.name,
